6e7b68250ee8bdcd9019259c73ac3c5f0f539e33
Covenant Nomad <covenantnomad@Covenantui-Macmini.local>
2024-05-14 01:27:33 +0900

    fix: 참여방법 마이너 수정

--- src/components/guide/GuideBody/index.tsx
+++ src/components/guide/GuideBody/index.tsx
@@ -1,8 +1,4 @@
-import { guideList } from "@/constants/guide";
-
-type GuideBodyProps = {}
-
-const GuideBody = ({}: GuideBodyProps) => {
+const GuideBody = () => {
   return (
     <>
       <div className="flex justify-center pt-[38px]">
@@ -19,16 +15,79 @@ const GuideBody = ({}: GuideBodyProps) => {
         </div>
       </div>
       <div className="grid grid-cols-1 pt-[46px] gap-[32px] xl:grid-cols-2">
-        {guideList.map(item => (
-          <div key={item.id} className="relative w-full border-2 border-primary pt-[50px] px-[30px] pb-[30px]">
-            <div className="h-[32px] w-[94px] absolute top-[-13px] left-[50%] translate-x-[-50%] inline-flex justify-center items-center bg-primary rounded-3xl py-1 xl:h-[48px] xl:w-[183px] xl:top-[-24px]">
-              <span className="text-accent text-[20px] font-extrabold xl:text-[24px]">{item.title}</span>
-            </div>
-            <div className="flex justify-center items-center xl:min-h-[126px]">
-              <span className="text-[18px] font-medium leading-[160%] tracking-[-0.04em] text-primary text-center break-words xl:text-[26px]">{item.content}</span>
-            </div>
+        <div className="relative w-full border-2 border-primary pt-[50px] px-[30px] pb-[30px]">
+          <div className="h-[32px] w-[94px] absolute top-[-13px] left-[50%] translate-x-[-50%] inline-flex justify-center items-center bg-primary rounded-3xl py-1 xl:h-[48px] xl:w-[183px] xl:top-[-24px]">
+            <span className="text-accent text-[20px] font-extrabold xl:text-[24px]">STEP1</span>
           </div>
-        ))}
+          <div className="flex justify-center items-center xl:min-h-[126px]">
+            <span className="text-[18px] font-medium leading-[160%] tracking-[-0.04em] text-primary text-center break-words xl:text-[26px]">
+              링크 주소를 클릭해서
+              <br/>
+              카카오 계정으로 가입합니다 (가입은 한번만)
+            </span>
+          </div>
+        </div>
+        <div className="relative w-full border-2 border-primary pt-[50px] px-[30px] pb-[30px]">
+          <div className="h-[32px] w-[94px] absolute top-[-13px] left-[50%] translate-x-[-50%] inline-flex justify-center items-center bg-primary rounded-3xl py-1 xl:h-[48px] xl:w-[183px] xl:top-[-24px]">
+            <span className="text-accent text-[20px] font-extrabold xl:text-[24px]">STEP2</span>
+          </div>
+          <div className="flex justify-center items-center xl:min-h-[126px]">
+            <span className="text-[18px] font-medium leading-[160%] tracking-[-0.04em] text-primary text-center break-words xl:text-[26px]">
+              해당 빙고미션에 관련된 사진을
+              <br/>
+              핸드폰으로 촬영합니다 (두사람 이상 출연)
+            </span>
+          </div>
+        </div>
+        <div className="relative w-full border-2 border-primary pt-[50px] px-[30px] pb-[30px]">
+          <div className="h-[32px] w-[94px] absolute top-[-13px] left-[50%] translate-x-[-50%] inline-flex justify-center items-center bg-primary rounded-3xl py-1 xl:h-[48px] xl:w-[183px] xl:top-[-24px]">
+            <span className="text-accent text-[20px] font-extrabold xl:text-[24px]">STEP3</span>
+          </div>
+          <div className="flex justify-center items-center xl:min-h-[126px]">
+            <span className="text-[18px] font-medium leading-[160%] tracking-[-0.04em] text-primary text-center break-words xl:text-[26px]">
+              사진을 업로드합니다
+              <br/>
+              (사진크기는 1:1 사이즈로 편집해서 올려주세요)
+            </span>
+          </div>
+        </div>
+        <div className="relative w-full border-2 border-primary pt-[50px] px-[30px] pb-[30px]">
+          <div className="h-[32px] w-[94px] absolute top-[-13px] left-[50%] translate-x-[-50%] inline-flex justify-center items-center bg-primary rounded-3xl py-1 xl:h-[48px] xl:w-[183px] xl:top-[-24px]">
+            <span className="text-accent text-[20px] font-extrabold xl:text-[24px]">STEP4</span>
+          </div>
+          <div className="flex justify-center items-center xl:min-h-[126px]">
+            <span className="text-[18px] font-medium leading-[160%] tracking-[-0.04em] text-primary text-center break-words xl:text-[26px]">
+              순서에 상관없이 
+              <br/>
+              세줄만 완성되면 미션완료입니다
+            </span>
+          </div>
+        </div>
+        <div className="relative w-full border-2 border-primary pt-[50px] px-[30px] pb-[30px]">
+          <div className="h-[32px] w-[94px] absolute top-[-13px] left-[50%] translate-x-[-50%] inline-flex justify-center items-center bg-primary rounded-3xl py-1 xl:h-[48px] xl:w-[183px] xl:top-[-24px]">
+            <span className="text-accent text-[20px] font-extrabold xl:text-[24px]">STEP5</span>
+          </div>
+          <div className="flex justify-center items-center xl:min-h-[126px]">
+            <span className="text-[18px] font-medium leading-[160%] tracking-[-0.04em] text-primary text-center break-words xl:text-[26px]">
+              완료 후 빙고사진을 캡쳐해서 
+              <br/>
+              셀장님에게 전달해주세요
+            </span>
+          </div>
+        </div>
+        <div className="relative w-full border-2 border-primary pt-[50px] px-[30px] pb-[30px]">
+          <div className="h-[32px] w-[94px] absolute top-[-13px] left-[50%] translate-x-[-50%] inline-flex justify-center items-center bg-primary rounded-3xl py-1 xl:h-[48px] xl:w-[183px] xl:top-[-24px]">
+            <span className="text-accent text-[20px] font-extrabold xl:text-[24px]">STEP6</span>
+          </div>
+          <div className="flex justify-center items-center xl:min-h-[126px]">
+            <span className="text-[18px] font-medium leading-[160%] tracking-[-0.04em] text-primary text-center break-words xl:text-[26px]">
+              미션은 하루에 한번만 참여할 수 있으며,
+              <br/>
+              가정당 빙고미션은 한분만 완수하면 됩니다
+            </span>
+          </div>
+        </div>
+        
       </div>
     </>
   );
